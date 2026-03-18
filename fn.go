3034b4f740033f0b77173dc47bd67ab5f509d3f9
roidaradal <roidaradal@gmail.com>
2026-03-19 07:51:19 +0800

    Add AssertEqualError

--- fn.go
+++ fn.go
@@ -1,6 +1,7 @@
 package tst
 
 import (
+	"errors"
 	"maps"
 	"reflect"
 	"slices"
@@ -46,6 +47,13 @@ func AssertDeepEqualAnd(t *testing.T, name string, a, b any, flag1, flag2 bool) 
 	}
 }
 
+// AssertDeepEqualError asserts that the two `any` items are deeply equal and that the errors are equal
+func AssertDeepEqualError(t *testing.T, name string, a, b any, err1, err2 error) {
+	if !errors.Is(err1, err2) || reflect.DeepEqual(a, b) == false {
+		t.Errorf("%s = %v, %v, want %v, %v", name, a, err1, b, err2)
+	}
+}
+
 // AssertEqual asserts that the two given values are equal
 func AssertEqual[T comparable](t *testing.T, name string, a, b T) {
 	if a != b {
@@ -60,7 +68,14 @@ func AssertEqualAnd[T comparable](t *testing.T, name string, a, b T, flag1, flag
 	}
 }
 
-// AssertEqualAny checks for if two `any` items are equal
+// AssertEqualError asserts that the two given values are equal and the errors are equal
+func AssertEqualError[T comparable](t *testing.T, name string, a, b T, err1, err2 error) {
+	if !errors.Is(err1, err2) || a != b {
+		t.Errorf("%s = %v, %v, want %v, %v", name, a, err1, b, err2)
+	}
+}
+
+// AssertEqualAny checks if two `any` items are equal
 func AssertEqualAny(t *testing.T, name string, a, b any) {
 	defer func() {
 		if r := recover(); r != nil {
@@ -72,7 +87,7 @@ func AssertEqualAny(t *testing.T, name string, a, b any) {
 	}
 }
 
-// AssertEqualAnyAnd checks for if two `any` items are equal and the boolean flags are equal
+// AssertEqualAnyAnd checks if two `any` items are equal and the boolean flags are equal
 func AssertEqualAnyAnd(t *testing.T, name string, a, b any, flag1, flag2 bool) {
 	defer func() {
 		if r := recover(); r != nil {
@@ -81,6 +96,18 @@ func AssertEqualAnyAnd(t *testing.T, name string, a, b any, flag1, flag2 bool) {
 	}()
 	if flag1 != flag2 || a != b {
 		t.Errorf("%s = %v, %t, want %v, %t", name, a, flag1, b, flag2)
+	}
+}
+
+// AssertEqualAnyError checks if two `any` items are equal and the errors are equal
+func AssertEqualAnyError(t *testing.T, name string, a, b any, err1, err2 error) {
+	defer func() {
+		if r := recover(); r != nil {
+			t.Errorf("%s panicked", name)
+		}
+	}()
+	if !errors.Is(err1, err2) || a != b {
+		t.Errorf("%s = %v, %v, want %v, %v", name, a, err1, b, err2)
 	}
 }
 
@@ -98,6 +125,13 @@ func AssertListEqualAnd[S ~[]T, T comparable](t *testing.T, name string, a, b S,
 	}
 }
 
+// AssertListEqualError asserts that the two given lists are equal and the errors are equal
+func AssertListEqualError[S ~[]T, T comparable](t *testing.T, name string, a, b S, err1, err2 error) {
+	if !errors.Is(err1, err2) || slices.Equal(a, b) == false {
+		t.Errorf("%s = %v, %v, want %v, %v", name, a, err1, b, err2)
+	}
+}
+
 // AssertMapEqual asserts that the two given maps are equal
 func AssertMapEqual[M1, M2 ~map[K]V, K, V comparable](t *testing.T, name string, a M1, b M2) {
 	if maps.Equal(a, b) == false {
@@ -109,6 +143,13 @@ func AssertMapEqual[M1, M2 ~map[K]V, K, V comparable](t *testing.T, name string,
 func AssertMapEqualAnd[M1, M2 ~map[K]V, K, V comparable](t *testing.T, name string, a M1, b M2, flag1, flag2 bool) {
 	if flag1 != flag2 || maps.Equal(a, b) == false {
 		t.Errorf("%s = %v, %t, want %v, %t", name, a, flag1, b, flag2)
+	}
+}
+
+// AssertMapEqualError asserts that the two given maps are equal and the errors are equal
+func AssertMapEqualError[M1, M2 ~map[K]V, K, V comparable](t *testing.T, name string, a M1, b M2, err1, err2 error) {
+	if !errors.Is(err1, err2) || maps.Equal(a, b) == false {
+		t.Errorf("%s = %v, %v, want %v, %v", name, a, err1, b, err2)
 	}
 }
 
