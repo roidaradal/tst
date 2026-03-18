27d91fd532825900f561412ae8a5bb92ea6630b3
roidaradal <roidaradal@gmail.com>
2026-03-18 13:23:03 +0800

    Add P1W2

--- tst.go
+++ tst.go
@@ -15,6 +15,12 @@ type P1W1[I, R any] struct {
 	W1 R
 }
 
+type P1W2[I, R1, R2 any] struct {
+	P1 I
+	W1 R1
+	W2 R2
+}
+
 type P2W1[I1, I2, R any] struct {
 	P1 I1
 	P2 I2
@@ -46,34 +52,45 @@ type P3W1Post[I1, I2, I3, R any] struct {
 // AllCompare1 tests 1 pair of [actual, want] from the given generic test cases
 func AllCompare1[T, R any](t *testing.T, testCases []T, name string, testFn func(T) (R, R), assert assertFn[R]) {
 	for i, x := range testCases {
-		actual, want := testFn(x)
 		label := fmt.Sprintf("%s:%d", name, i)
+		actual, want := testFn(x)
 		assert(t, label, actual, want)
 	}
 }
 
 // AllActionPost performs the action for all generic test cases and checks the post test
 func AllActionPost[T HasPostTest](t *testing.T, testCases []T, name string, actionFn func(T)) {
-	for _, x := range testCases {
+	for i, x := range testCases {
+		label := fmt.Sprintf("%s:%d", name, i)
 		actionFn(x)
-		assertTest(t, name, x.PostTest)
+		assertTest(t, label, x.PostTest)
 	}
 }
 
 // AllP1W1 tests all P1W1 test cases
 func AllP1W1[I, R any](t *testing.T, testCases []P1W1[I, R], name string, testFn func(I) R, assert assertFn[R]) {
 	for i, x := range testCases {
-		actual := testFn(x.P1)
 		label := fmt.Sprintf("%s:%d", name, i)
+		actual := testFn(x.P1)
 		assert(t, label, actual, x.W1)
+	}
+}
+
+// AllP1W2 tests all P1W2 test cases
+func AllP1W2[I, R1, R2 any](t *testing.T, testCases []P1W2[I, R1, R2], name string, testFn func(I) (R1, R2), assert1 assertFn[R1], assert2 assertFn[R2]) {
+	for i, x := range testCases {
+		label := fmt.Sprintf("%s:%d", name, i)
+		actual1, actual2 := testFn(x.P1)
+		assert1(t, label, actual1, x.W1)
+		assert2(t, label, actual2, x.W2)
 	}
 }
 
 // AllP2W1 tests all P2W1 test cases
 func AllP2W1[I1, I2, R any](t *testing.T, testCases []P2W1[I1, I2, R], name string, testFn func(I1, I2) R, assert assertFn[R]) {
 	for i, x := range testCases {
-		actual := testFn(x.P1, x.P2)
 		label := fmt.Sprintf("%s:%d", name, i)
+		actual := testFn(x.P1, x.P2)
 		assert(t, label, actual, x.W1)
 	}
 }
@@ -81,8 +98,8 @@ func AllP2W1[I1, I2, R any](t *testing.T, testCases []P2W1[I1, I2, R], name stri
 // AllP2W2 tests all P2W2 test cases
 func AllP2W2[I1, I2, R1, R2 any](t *testing.T, testCases []P2W2[I1, I2, R1, R2], name string, testFn func(I1, I2) (R1, R2), assert1 assertFn[R1], assert2 assertFn[R2]) {
 	for i, x := range testCases {
-		actual1, actual2 := testFn(x.P1, x.P2)
 		label := fmt.Sprintf("%s:%d", name, i)
+		actual1, actual2 := testFn(x.P1, x.P2)
 		assert1(t, label, actual1, x.W1)
 		assert2(t, label, actual2, x.W2)
 	}
@@ -91,8 +108,8 @@ func AllP2W2[I1, I2, R1, R2 any](t *testing.T, testCases []P2W2[I1, I2, R1, R2],
 // AllP3W1 tests all P3W1 test cases
 func AllP3W1[I1, I2, I3, R any](t *testing.T, testCases []P3W1[I1, I2, I3, R], name string, testFn func(I1, I2, I3) R, assert assertFn[R]) {
 	for i, x := range testCases {
-		actual := testFn(x.P1, x.P2, x.P3)
 		label := fmt.Sprintf("%s:%d", name, i)
+		actual := testFn(x.P1, x.P2, x.P3)
 		assert(t, label, actual, x.W1)
 	}
 }
@@ -100,8 +117,8 @@ func AllP3W1[I1, I2, I3, R any](t *testing.T, testCases []P3W1[I1, I2, I3, R], n
 // AllP3W1Post tests all P3W1Post test cases and checks the post-test function
 func AllP3W1Post[I1, I2, I3, R any](t *testing.T, testCases []P3W1Post[I1, I2, I3, R], name string, testFn func(I1, I2, I3) R, assert assertFn[R]) {
 	for i, x := range testCases {
-		actual := testFn(x.P1, x.P2, x.P3)
 		label := fmt.Sprintf("%s:%d", name, i)
+		actual := testFn(x.P1, x.P2, x.P3)
 		assert(t, label, actual, x.W1)
 		assertTest(t, label, x.Test)
 	}
