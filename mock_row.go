ae7f9e73a4dfa716c5d1a9ab5a06303d95dc24b8
roidaradal <roidaradal@gmail.com>
2026-03-24 14:29:37 +0800

    Add mock Rows

--- mock_row.go
+++ mock_row.go
@@ -9,8 +9,18 @@ type Row struct {
 	items []any
 }
 
+type Rows struct {
+	items [][]any
+	index int
+	err   error
+}
+
 func NewRow(items ...any) *Row {
 	return new(Row{items: items})
+}
+
+func NewRows(items ...[]any) *Rows {
+	return new(Rows{items: items, index: 0, err: nil})
 }
 
 func (r *Row) Scan(fieldRefs ...any) (err error) {
@@ -26,5 +36,41 @@ func (r *Row) Scan(fieldRefs ...any) (err error) {
 		fieldValue := reflect.ValueOf(fieldRef).Elem()
 		fieldValue.Set(reflect.ValueOf(r.items[i]))
 	}
+	return err
+}
+
+func (r *Rows) Close() error {
+	return nil
+}
+
+func (r *Rows) Err() error {
+	return r.err
+}
+
+func (r *Rows) SetError(err error) {
+	r.err = err
+}
+
+func (r *Rows) Next() bool {
+	return r.index < len(r.items)
+}
+
+func (r *Rows) Scan(fieldRefs ...any) (err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = fmt.Errorf("panic encountered: %v", r)
+		}
+	}()
+	if !r.Next() {
+		return fmt.Errorf("no more rows")
+	}
+	if len(fieldRefs) != len(r.items[r.index]) {
+		return fmt.Errorf("expected %d fieldRefs, got %d", len(r.items[r.index]), len(fieldRefs))
+	}
+	for i, fieldRef := range fieldRefs {
+		fieldValue := reflect.ValueOf(fieldRef).Elem()
+		fieldValue.Set(reflect.ValueOf(r.items[r.index][i]))
+	}
+	r.index += 1
 	return err
 }
